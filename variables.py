51131c8a65f756e80a97d11d3107786acf9e29f6
Baraa Mohammad <bmohammad@math-pc70.mathematik.uni-kassel.de>
2025-04-21 18:14:59 +0200

    Adding Variables Files!!

---
+++ variables.py
@@ -0,0 +1,28 @@
+# Variables
+
+
+company = "Harisystems"
+
+print(company)
+
+first_name = "Baraa"
+last_name = "Mohammad"
+
+print(first_name + " " + last_name)
+
+# rules
+# Letters (A-Z, a-z, 0-9 and _ )
+# don't start with number
+# No spaces on variable name
+
+
+import keyword
+
+res = keyword.kwlist
+
+print(res)
+
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break',
+# 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally',
+# 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal',
+# 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
