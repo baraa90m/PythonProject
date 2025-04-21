1f3e9010418b58f44c8a1274c591d428eda144cc
Baraa Mohammad <bmohammad@math-pc70.mathematik.uni-kassel.de>
2025-04-21 18:46:51 +0200

    Adding Datatypes File!!

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+# DataTypes
+
+#String - str
+name = "Max"
+print(type(name))
+
+#Integer - int
+num = 21
+print(type(num))
+
+#Float
+price = 30.22
+print(type(price))
+
+#Boolean - bool - True/False
+rainy_day = True
+print(type(rainy_day))
+
+a = "5"
+b = "4"
+c = a + b
+print(c)
