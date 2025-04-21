2762e612930b229640f3f03fe97cc1ab6651b942
Baraa Mohammad <bmohammad@math-pc70.mathematik.uni-kassel.de>
2025-04-21 21:15:31 +0200

    Adding the File of String Built in Function!!

---
+++ string_builtin_functions.py
@@ -0,0 +1,12 @@
+# String Built in Functions
+# Function and Method
+
+course = "Python for Beginners"
+print(len(course))
+
+# Method - Examples:
+
+print(course.upper())
+print(course.lower())
+print(course.replace("Beginners", "Starters"))
+print("Python" in course)
