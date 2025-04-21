cabc6fb19dff2eca87652a3b4c0b89a9eba9be23
Baraa Mohammad <bmohammad@math-pc70.mathematik.uni-kassel.de>
2025-04-21 19:29:24 +0200

    Adding String File!!

---
+++ strings.py
@@ -0,0 +1,15 @@
+# String
+
+course = "Python  "  \
+    "for " \
+    "Beginners" \
+
+print(course)
+
+letter = '''
+Dear Student,
+Tody you are learning python
+from harisystem IT Group
+'''
+
+print(letter)
