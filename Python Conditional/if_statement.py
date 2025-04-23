e0281b53e4a951c94dd9fbce6729098119372ea7
Baraa Mohammad <bmohammad@math-pc70.mathematik.uni-kassel.de>
2025-04-23 18:51:42 +0200

    Adding files of python conditional!!

---
+++ Python Conditional/if_statement.py
@@ -0,0 +1,10 @@
+# If Statements
+
+is_hot = False
+is_cold = False
+if is_hot:
+    print("It is hot")
+elif is_cold:
+    print("It is cold")
+else:
+    print("It is A normal day")
---
+++ Python Conditional/if_statement_coding_exercise.py
@@ -0,0 +1,12 @@
+# If Statement
+# Coding Exercise
+
+x = 20
+y = 20
+
+if x > y:
+    print("X is greeter than Y.")
+elif x == y:
+    print("X is equal to Y.")
+else:
+    print("Y is greeter than X.")
