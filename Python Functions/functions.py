49991fd62f4258365f55913a5269ce4fce546e88
Baraa Mohammad <bmohammad@math-pc70.mathematik.uni-kassel.de>
2025-04-24 14:51:48 +0200

    Python Functions files are added

---
+++ Python Functions/functions.py
@@ -0,0 +1,15 @@
+# Functions
+
+def greet_user():
+    print("Hi there")
+    print("Welcome to Harisystems")
+
+greet_user()
+
+def multiplication(val1, val2):
+    res = val1 * val2
+    return res
+
+sol = multiplication(2, 55)
+print(sol)
+
---
+++ Python Functions/functions_with_arguments.py
@@ -0,0 +1,7 @@
+# Functions with Arguments
+
+def greet_user(name, lname):
+    print(f"Hello {name} {lname}!")
+    print("Welcome to Python Programming Course")
+
+greet_user("Max", "Mustermann")
