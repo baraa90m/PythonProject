9890bd73985165995e579f158b5fbd050da483c2
Baraa Mohammad <bmohammad@math-pc70.mathematik.uni-kassel.de>
2025-04-25 16:37:17 +0200

    class_program is modified!!

--- Python Object Oriented Programming OOP/class_program.py
+++ Python Object Oriented Programming OOP/class_program.py
@@ -1,15 +1,30 @@
-# Class Program
+# Creating a class
 
 class Greet():
 
-    # class attribute
-    course = "Python for beginners"
+    course = "Python for beginners"  # class attribute
 
     def greet_user(self):
         print("Hello Developer")
 
+# Class Constructors: We have two types of class constructors
+# 1- Default Constructors:
+class Harisystems:
+    def __init__(self):
+        self.course = "Python for beginners"
+
+    def access(self):
+        print(self.course)
+
+
+# 2- Parametrised Constructor:
+
+
+
 # Creating a local object and Instantiating a class object
 if __name__ == "__main__":
-    wish = Greet()
-    wish.greet_user()
-    print(wish.course)
+    #wish = Greet()
+    #wish.greet_user()
+    #print(wish.course)
+    obj = Harisystems()
+    obj.access()
